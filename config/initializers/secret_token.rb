2f902c16368f2663b6c9a5d6d24658dbfab6eaf9
Ankur Singh <ankur13019@iiitd.ac.in>
2014-04-03 02:18:16 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bpt2::Application.config.secret_token = '3f58c47e46920ec7a480527e73829a7ec0cbb62d4c4645ce49b3ad4d033a610a2a4c0353e835c53ac8001cf74724d2907b708c75f20f4233098a8202a065eb0f'
